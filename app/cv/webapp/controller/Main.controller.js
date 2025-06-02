eb0fc7d3be14beca8ea1bf13d4336d281da84be0
Mikalai Kazlou <mikalai_kazlou@epam.com>
2025-06-02 09:53:32 +0300

    feat: add WorkExperience fragment

--- app/cv/webapp/controller/Main.controller.js
+++ app/cv/webapp/controller/Main.controller.js
@@ -1,5 +1,38 @@
-sap.ui.define(["./BaseController"], function (BaseController) {
+sap.ui.define(["./BaseController", "sap/ui/model/json/JSONModel"], function (BaseController, JSONModel) {
   "use strict";
 
-  return BaseController.extend("com.exercise.cvsapui5.controller.Main", {});
+  return BaseController.extend("com.exercise.cvsapui5.controller.Main", {
+    onInit: function () {
+      const oViewModel = new JSONModel();
+      this.setModel(oViewModel, "strings");
+    },
+
+    onWorkExperienceListUpdateFinished: function (oEvent) {
+      const oList = oEvent.getSource();
+      const aItems = oList.getItems();
+      const oData = [];
+
+      aItems.forEach((oItem) => {
+        const oItemData = oItem.getBindingContext("mockdata").getObject();
+
+        oData.push({
+          sTechnologies: this._convertListToSortedString(oItemData.technologies),
+          sProjectRoles: this._convertListToSortedString(oItemData.projectRoles),
+        });
+
+        oItem.bindElement({
+          path: `/${oData.length - 1}`,
+          model: "strings",
+        });
+      });
+
+      this.getModel("strings").setData(oData);
+    },
+
+    _convertListToSortedString: function (aList) {
+      aList.sort((a, b) => a.order - b.order);
+      const aTitles = aList.map((oList) => oList.child.title);
+      return aTitles.join(", ");
+    },
+  });
 });
